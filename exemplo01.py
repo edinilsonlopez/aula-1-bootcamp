bd8fcb90463419d5e4b8cc6f7d5cac18b409d0b0
Edinilson Lopez <Edinilson>
2025-01-25 14:43:15 -0300

    Dois exemplos basicos

--- exemplo01.py
+++ exemplo01.py
@@ -1,1 +1,3 @@
-print("exemplo 1 - aula1")
+nome = input("Digite seu nome:")
+lenght = len(nome)
+print('Ola ' + nome + 'seu nome tem ' + str(lenght) + ' letras')
---
+++ exemplo02.py
@@ -0,0 +1,3 @@
+val1 = input("Digite o primeiro valor: ")
+val2 = input("Ditite o valor a somar: ")
+print("O resultado da soma dos dois valores é:" + str(int(val1) + int(val2)))
